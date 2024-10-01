72db4cd659edd930562c914c897bcb03be1f8fbd
marcelcamposbento <142677314+marcelcamposbento@users.noreply.github.com>
2024-10-01 20:45:54 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,55 @@
+let dados = [
+    {
+      titulo: "Harry Potter e a Pedra Filosofal ",
+      descricao: "Harry Potter e a Pedra Filosofal: Harry, um órfão criado por tios bruxos, descobre seu passado mágico e é convidado para estudar em Hogwarts.",
+      link: "https://www.youtube.com/watch?v=9fIObnIGMlI",
+      tags:"pedra"
+    },
+    {
+      titulo: "Harry Potter e a Câmera Secreta",
+      descricao: "Harry Potter e a Câmara Secreta: Alunos de Hogwarts estão sendo petrificados, e Harry é o principal suspeito.",
+      link: "https://www.youtube.com/watch?v=cf0rRYvDuys",
+      tags:"secreta"
+    },
+    
+    {
+      titulo: "Harry Potter e o Prisioneiro de Azkaban",
+      descricao: "Harry Potter e o Prisioneiro de Azkaban: Um perigoso fugitivo da prisão de Azkaban ameaça a vida de Harry.",
+      link: "https://www.youtube.com/watch?v=e02fwhC6DiQ",
+      tags:"Azkaban"
+    },
+    {
+      titulo: "Harry Potter e o Cálice de Fogo",
+      descricao: "Harry Potter e o Cálice de Fogo: Harry é misteriosamente escolhido para participar do Torneio Tribruxo, uma competição entre três escolas de magia.",
+      link: "https://www.youtube.com/watch?v=RwJlOdtoHRE",
+      tags:"cálice"
+    },
+    {
+        titulo: "Harry Potter e a Ordem da Fênix",
+        descricao: "Harry Potter e a Ordem da Fênix: O Ministério da Magia nega a volta de Voldemort e tenta silenciar Dumbledore.Harry lidera um exército de alunos para se defender contra as forças das trevas.",
+        link: "https://www.youtube.com/watch?v=Tbv2Al6LGW8",
+        tags:"fênix"
+      },
+      {
+        titulo: "Harry Potter e o Enigma do Príncipe",
+        descricao: "Harry Potter e o Enigma do Príncipe: Harry encontra um antigo livro de poções que contém notas de um antigo diretor de Hogwarts.Enquanto isso, Voldemort se fortalece e a guerra se aproxima.",
+        link: "https://www.youtube.com/watch?v=wgkHfUaG1nI",
+        tags:"enigma"
+
+      },
+      {
+        titulo: "Harry Potter e as Relíquias da Morte - Parte 1",
+        descricao: "Harry Potter e as Relíquias da Morte - Parte 1: Harry, Rony e Hermione embarcam em uma perigosa missão para encontrar as Horcruxes de Voldemort e destruí-las.A busca os leva a lugares perigosos e revela segredos do passado.",
+        link: "https://www.youtube.com/watch?v=-_GftUTRWHM",
+        tags:"relíquias"
+
+      },
+      {
+        titulo: "Harry Potter e as Relíquias da Morte - Parte 2",
+        descricao: "Harry Potter e as Relíquias da Morte - Parte 2: A batalha final entre o bem e o mal se aproxima.Harry e seus amigos enfrentam Voldemort em uma batalha épica em Hogwarts.",
+        link: "https://www.youtube.com/watch?v=E79fa8bLfPA",
+        tags:"morte"
+
+      }
+];
+
